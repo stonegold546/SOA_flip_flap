ec9c099466ccd4a80716ef3deb7a6984b1516731
James Uanhoro <stonegold546@gmail.com>
2015-10-01 15:35:09 +0800

    Done with to_yml

--- flip_flap.rb
+++ flip_flap.rb
@@ -1,6 +1,14 @@
+require 'yaml'
 require_relative 'tsv_buddy'
 
+# FlipFlap Class
 class FlipFlap
   # Do NOT create an initialize method
+  include TsvBuddy
 
+  attr_accessor :data
+
+  def to_yaml
+    @data.to_yaml
+  end
 end
--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -7,7 +7,20 @@ module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
+    lines = tsv.split("\n")
+    keys = lines[0].split("\t")
+    @data = lines[1..-1].map do |line|
+      Hash[*take_tsv_helper(keys, line)]
+    end.compact
+  end
 
+  def take_tsv_helper(keys, line)
+    output = []
+    line.split("\t").each_with_index do |value, index|
+      output << keys[index]
+      output << value
+    end
+    output
   end
 
   # to_tsv: converts @data into tsv string
